b03f4641809b0a3896857175b59f255e94448061
Nasim Beigzadeh <159526438+NasimBeigzadeh@users.noreply.github.com>
2026-09-07 19:08:31 +0330

    Update dataset.py

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -1,1 +1,71 @@
+# -*- coding: utf-8 -*-
 
+"""
+Dataset loader for ECG image classification.
+
+Supports:
+- PTB-XL
+- Chapman
+"""
+
+
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+
+
+def create_dataloader(
+        train_path,
+        valid_path,
+        test_path,
+        train_transform,
+        valid_transform,
+        test_transform,
+        batch_size=128
+):
+
+
+    train_dataset = ImageFolder(
+        train_path,
+        transform=train_transform
+    )
+
+
+    valid_dataset = ImageFolder(
+        valid_path,
+        transform=valid_transform
+    )
+
+
+    test_dataset = ImageFolder(
+        test_path,
+        transform=test_transform
+    )
+
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+
+    valid_loader = DataLoader(
+        valid_dataset,
+        batch_size=batch_size,
+        shuffle=False
+    )
+
+
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False
+    )
+
+
+    return (
+        train_loader,
+        valid_loader,
+        test_loader
+    )
