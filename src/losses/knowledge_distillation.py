29c5448b0ae16f44d4d7bd8e650f8a11a24c18d8
Nasim Beigzadeh <159526438+NasimBeigzadeh@users.noreply.github.com>
2026-09-07 19:13:29 +0330

    Update knowledge_distillation.py

--- src/losses/knowledge_distillation.py
+++ src/losses/knowledge_distillation.py
@@ -1,1 +1,52 @@
+class MultiTeacherKDLoss(nn.Module):
 
+    def __init__(
+            self,
+            temperature=2.0,
+            alpha=0.2
+    ):
+        super().__init__()
+
+        self.temperature = temperature
+        self.alpha = alpha
+
+        self.ce_loss = nn.CrossEntropyLoss()
+
+
+    def forward(
+            self,
+            student_output,
+            teacher_output,
+            labels
+    ):
+
+        T = self.temperature
+
+
+        kd_loss = nn.KLDivLoss(
+            reduction="batchmean"
+        )(
+            F.log_softmax(
+                student_output / T,
+                dim=1
+            ),
+
+            F.softmax(
+                teacher_output / T,
+                dim=1
+            )
+        )
+
+
+        kd_loss = self.alpha * kd_loss
+
+
+        ce_loss = (
+            1 - self.alpha
+        ) * self.ce_loss(
+            student_output,
+            labels
+        )
+
+
+        return kd_loss + ce_loss
